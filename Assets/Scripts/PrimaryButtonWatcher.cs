428ab08c2afcf857146a3a83776a654d52bfc8a5
Sander Standaert <s112883@ap.be>
2021-12-30 14:26:07 +0100

    Device finder fix?

--- Assets/Scripts/PrimaryButtonWatcher.cs
+++ Assets/Scripts/PrimaryButtonWatcher.cs
@@ -11,8 +11,9 @@ public class PrimaryButtonWatcher : MonoBehaviour
 {
     public PrimaryButtonEvent primaryButtonDown;
     private bool previousButtonState = false;
-    private List<InputDevice> leftHandedDevices;
-    private List<InputDevice> rightHandedDevices;
+    private List<InputDevice> devicesPrimaryButton;
+    //private List<InputDevice> leftHandedDevices;
+    //private List<InputDevice> rightHandedDevices;
 
     private void Awake()
     {
@@ -21,44 +22,76 @@ public class PrimaryButtonWatcher : MonoBehaviour
             primaryButtonDown = new PrimaryButtonEvent();
         }
 
-        leftHandedDevices = new List<InputDevice>();
-        rightHandedDevices = new List<InputDevice>();
+
+        devicesPrimaryButton = new List<InputDevice>();
+        //leftHandedDevices = new List<InputDevice>();
+        //rightHandedDevices = new List<InputDevice>();
     }
     private void OnEnable()
     {
-        InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, leftHandedDevices);
-        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandedDevices);
+        //InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, leftHandedDevices);
+        //InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandedDevices);
 
-        if (leftHandedDevices.Count > 1 || rightHandedDevices.Count > 1)
+        List<InputDevice> allDevices = new List<InputDevice>();
+        InputDevices.GetDevices(allDevices);
+        foreach (InputDevice device in allDevices)
         {
-            Debug.Log("More than 1 left/right handed device found");
+            InputDevices_Connected(device);
         }
-        else if(leftHandedDevices.Count == 0 || rightHandedDevices.Count == 0)
+
+        InputDevices.deviceConnected += InputDevices_Connected;
+        InputDevices.deviceDisconnected += InputDevices_Disconnected;
+
+        //if (leftHandedDevices.Count > 1 || rightHandedDevices.Count > 1)
+        //{
+        //    Debug.Log("More than 1 left/right handed device found");
+        //}
+        //else if(leftHandedDevices.Count == 0 || rightHandedDevices.Count == 0)
+        //{
+        //    Debug.Log("none found");
+        //    Debug.Log(rightHandedDevices.Count);
+        //}
+        //else
+        //{
+        //    Debug.Log("error");
+        //}
+    }
+
+    void InputDevices_Connected(InputDevice device)
+    {
+        bool discardedValue;
+        if (device.TryGetFeatureValue(CommonUsages.primaryButton, out discardedValue))
         {
-            Debug.Log("none found");
-            Debug.Log(rightHandedDevices.Count);
+            devicesPrimaryButton.Add(device);
         }
-        else
+    }
+
+    void InputDevices_Disconnected(InputDevice device)
+    {
+        if (devicesPrimaryButton.Contains(device))
         {
-            Debug.Log("error");
+            devicesPrimaryButton.Remove(device);
         }
     }
 
     private void OnDisable()
     {
-        rightHandedDevices.Clear();
-        leftHandedDevices.Clear();
+        InputDevices.deviceConnected -= InputDevices_Connected;
+        InputDevices.deviceDisconnected -= InputDevices_Disconnected;
+        devicesPrimaryButton.Clear();
+        //rightHandedDevices.Clear();
+        //leftHandedDevices.Clear();
     }
 
     private void Update()
     {
 
         bool tempstate = false;
-        foreach (var rightHand in rightHandedDevices)
+        foreach (var device in devicesPrimaryButton)
         {
             bool primaryButtonState = false;
             // If we got a value and this is true then primaryButton is pressed, tempstate has to be false or else it's being held down
-            tempstate = rightHand.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonState) && primaryButtonState || tempstate;
+            tempstate = device.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonState) && primaryButtonState || tempstate;
         }
 
         if (tempstate != previousButtonState)
